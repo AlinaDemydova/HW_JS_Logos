473419316f4c57384b380280d31462eb54616e13
AlinaDemydova <44206758+AlinaDemydova@users.noreply.github.com>
2018-11-11 20:07:41 +0200

    Update js_13.js

--- js_13.js
+++ js_13.js
@@ -4,17 +4,7 @@ let btn = document.querySelector('button');
 
 btn.onclick = closeAlert;
 
-/*for (let i = 0; i<2; i++) {
-let btn = document.querySelectorAll('button')[i];
-}
-*/
 function closeAlert() {
-	/*for (let i = 0; i<2; i++) {
-document.querySelectorAll('button')[i].parentNode.style.display = 'none';
-}*/
 	document.querySelector('button').parentNode.style.display = 'none';
-	
-	//document.querySelector('div > div > button').style.display = 'none';
-	//document.getElementById('1button').style.display = 'none';
 }
 
