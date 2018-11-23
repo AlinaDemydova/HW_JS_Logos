70f77b41266c74bed1db602386ebaa45b3c2d119
AlinaDemydova <44206758+AlinaDemydova@users.noreply.github.com>
2018-11-23 18:19:27 +0200

    Update hw_17_18_watch.js

--- hw_17_18_watch.js
+++ hw_17_18_watch.js
@@ -1,5 +1,3 @@
-
-
 function ssArrow() {
 //створюємо канвас коробку
 let st = document.getElementById("myCanvas");
@@ -50,49 +48,3 @@ let timerId = setInterval(function() {
 }, 1000);
 }
 electroWarch();
-
-
-
-
-
-
-/*function eW() {
-
-let timerId = setInterval(function() {
-    let elWatch = moment().format('HH:mm:ss');
-		
-	div.write(elWatch);
-	
-}, 1000);
-};
-eW();*/
-
-/*var timerId;
-
-function clockStart() { // запустить часы
-  timerId = setInterval(update, 1000);
-  update(); // (*)
-}
-
-function clockStop() {
-  clearInterval(timerId);
-  timerId = null;
-}
-
-function update() {
-  var clock = document.getElementById('clock');
-
-  var date = new Date(); // (*)
-
-  var hours = date.getHours();
-  if (hours < 10) hours = '0' + hours;
-  clock.children[0].innerHTML = hours;
-
-  var minutes = date.getMinutes();
-  if (minutes < 10) minutes = '0' + minutes;
-  clock.children[1].innerHTML = minutes;
-
-  var seconds = date.getSeconds();
-  if (seconds < 10) seconds = '0' + seconds;
-  clock.children[2].innerHTML = seconds;
-}*/
