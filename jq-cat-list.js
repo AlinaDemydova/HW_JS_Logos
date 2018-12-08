e405eca2068b4cdf841ac7a2118b8719be5ddb69
Alina Demydova <web1demydova@gmail.com>
2018-12-08 10:16:38 +0200

    Update jq-cat-list.js

--- jq-cat-list.js
+++ jq-cat-list.js
@@ -1,6 +1,9 @@
+$(document).ready(function() {
+
 function addNewList() {
 	$('#of10').css('visibility', 'hidden');
 	$('body').append('<ul id="ulId"></ul>');
+	$('#ulId').sortable();
 	$('#text').click(function() {
 		$('#buttonPlus').attr('disabled', false);
 		$('#text').val('');
@@ -9,7 +12,7 @@ function addNewList() {
 	$('#buttonPlus').click(function newLine() {
 		if ($('#text').val() !== '') {
 			let listLength = $('li').length;
-			if (listLength < 3) {
+			if (listLength < 10) {
 				$('ul').append(
 					'<li class="liClass"><input type="checkbox" class="checkboxLi" value = "Add new action"/>' +
 					$('#text').val() + '<button class="buttonDeleteClass"></button></li>');
@@ -22,15 +25,16 @@ function addNewList() {
 			} else {
 				$('#of10').css('visibility', 'visible');
 				$('#text').val('Add new action');
+				$('#buttonPlus').attr('disabled', true);
 			}
 		} else {
-			$('#text').val('Add new action');
 			$('#buttonPlus').attr('disabled', false);
+			$('#text').val('Add new action');
 		}
 	});
 }
 addNewList();
 
-$(document).ready(function() {
-	$('#ulId').sortable();
+
+	
 });
